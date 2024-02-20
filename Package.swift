c0332ef8ff5df17fa6e0e16abe3f8f5b4932ffa3
Stanislau Baranouski <stan.baranouski@thechefz.co>
2024-02-20 13:41:35 +0300

    fixed SPM support

--- Package.swift
+++ Package.swift
@@ -5,16 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "RangeSeekSlider",
-    platforms: [.iOS(.v13)],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RangeSeekSlider",
             targets: ["RangeSeekSlider"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RangeSeekSlider",
-            path: "Sources"
-        )
+            name: "RangeSeekSlider", path: "Sources"),
+        .testTarget(
+            name: "RangeSeekSliderTests",
+            dependencies: ["RangeSeekSlider"]),
     ]
 )
